839e1bf8b36d029202b86d258012812cd18e00bf
ashwin-s-walunj <ashwinwalunj@gmail.com>
2026-02-27 22:47:10 +0100

    Added url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
